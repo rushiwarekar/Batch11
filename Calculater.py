4fea70a0f2f7cb6ce8026a746ff09e74f8fd3326
rushiwarekar <rushiwarekar@gmail.com>
2023-07-09 09:03:50 +0530

    cal added to rushi_dev

--- Calculater.py
+++ Calculater.py
@@ -5,3 +5,8 @@ def Addition(a,b):
     return a + b
 print("Addition of 2 num:",Addition(a,b))
 
+# subtraction function added in rushi_dev branch on git
+
+def Sub(a,b):
+    return a - b
+print("Subtrction of 2 num:",Sub(a, b))
